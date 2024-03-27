a5c89f8a5d6b3a2c22f5348e53c71908537338e2
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2024-03-27 18:11:24 +0200

    added mini_css_plugin, scss modules

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from "./components/Counter";
+import "./index.scss";
+
+const App = () => {
+    return (
+        <div className="app">
+            sasa
+            <Counter /> 
+        </div>
+    )
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-import { render } from 'react-dom';
-import { Counter } from './components/Counter';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-render(
-    <div>
-        <Counter/>
-    </div>,
-    document.getElementById('root')
-)
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
