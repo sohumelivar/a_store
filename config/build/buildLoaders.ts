38284e7c681096c8aaf0dc34b65588cc0c52a501
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2024-03-27 17:01:45 +0200

    added react, css-loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,7 +8,17 @@ export function buildLoaders(): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     };
 
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+        ],
+    }
+
     return [
         typescriptLoader,
+        cssLoader,
     ]
 }
