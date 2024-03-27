c14084f80a1683ece3606d31854ac08f91bec0e3
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2024-03-27 20:27:04 +0200

    added themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./components/Counter";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+ 
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>theme</button>
             <Link to={'/'}>Main Page</Link>
             <Link to={'/about'}>About Page</Link>
             <Link to={'/counter'}>Counter</Link>
