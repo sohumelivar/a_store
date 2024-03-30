94b69b2acba6bb37d27f0685120c90894a3c89cd
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2024-03-30 11:04:20 +0300

    implementation of feature sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import { Link, Route, Routes } from "react-router-dom";
-import { Counter } from "./components/Counter";
 import "./styles/index.scss";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense } from "react";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { classNames } from "shared/lib/classNames/classNames";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -18,9 +17,8 @@ const App = () => {
             <Link to={'/counter'}>Counter</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
-                    <Route path={'/counter'} element={<Counter />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
