3a3c7ba534d215268a140a6890e7307a2587c1ac
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2024-04-04 17:48:49 +0300

    bundle analyzer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,13 @@ import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
-import { Suspense } from "react";
-import { useTranslation } from "react-i18next";
+import { Suspense, useEffect } from "react";
 import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 import { LangSwitcher } from "widgets/LangSwitcher/LangSwitcher";
 
 const App = () => {
     const {theme} = useTheme();
- 
+
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
