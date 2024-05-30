0705f4389a1547c64f2f51f5dc98c64947063680
sohumelivar <sohumelivar1@gmail.com>
2024-05-30 19:45:10 +0300

    added login func

--- src/pages/RegistrationPage/api/registrationFormAPI.ts
+++ src/pages/RegistrationPage/api/registrationFormAPI.ts
@@ -3,8 +3,5 @@ import { RegistrationSchema } from "../model/types/RegestrationSchema";
 
 export const registrationFormAPI = async (props: RegistrationSchema) => {
     const response = await $api.post<RegistrationSchema>('/user/registrationForm', props);
-    if (response.data.error) {
-        console.log('error ------ >>>>>');
-    }
     return response.data;
 } 
