c8ee6d22b803894825ec8ca3075fff1d30f345f5
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2024-06-03 23:16:32 +0300

    fix reg, login api, added api interceptors

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -9,7 +9,39 @@ import axios from "axios";
 
 export const $api = axios.create({
     baseURL: 'http://localhost:5000/api',
-    headers: {
-        'Authorization': `test1`
+    withCredentials: true,
+    // headers: {
+    //     'Authorization': `Bearer ${localStorage.getItem('token')}`
+    // }
+});
+
+$api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-});    
+);
+
+$api.interceptors.response.use((config) => {
+    return config;
+}, async (error) => {
+    try {
+        const originalRequest = error.config;
+        console.log('originalRequest --- >>> ', originalRequest);
+
+        if (error.response.status === 401) {
+            const response = await axios.get('http://localhost:5000/api/user/refresh', { withCredentials: true });
+            localStorage.setItem('token', response.data.accessToken);
+            return $api.request(originalRequest);
+        }
+    } catch (error) {
+        console.log(error);
+
+    }
+})
